fcd45cddb6cd0b2229f0d455ff3a3a7f54d06603
David <davidemanuel.cf@gmail.com>
2020-08-11 21:04:26 -0300

    Finalizando API NLW 2

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,12 @@
-import knex from 'knex'
-import path from 'path'
+import knex from "knex";
+import path from "path";
 
 const db = knex({
-  client: 'sqlite3',
+  client: "sqlite3",
   connection: {
-    filename: path.resolve(__dirname, 'database.sqlite3')
+    filename: path.resolve(__dirname, "database.sqlite3"),
   },
-  useNullAsDefault: true
+  useNullAsDefault: true,
 });
 
 export default db;
